d4c9ce81d9c2fb4f5dd6317ae3f5992c2006164e
Taufiq Ahommed Neloy <48958588+Taufiq63569@users.noreply.github.com>
2019-03-29 12:42:21 +0600

    Update Selection.c

--- Sort/Selection sort/Selection.c
+++ Sort/Selection sort/Selection.c
@@ -2,35 +2,46 @@
 int main ()
 {
     int a[100], n,i;
+    printf("Size of array: ");
     scanf("%d",&n);
-    for(i=0;i<n;i++){
+    printf("\nTake input: ");
+    for(i=0; i<n; i++)
+    {
         scanf("%d",&a[i]);
     }
-
-     for(i=0;i<n;i++){
+    printf("\nBefore sort:  \n");
+    for(i=0; i<n; i++)
+    {
         printf("%d ",a[i]);
     }
-    printf("\n");
+    printf("\n\nAfter sort:  \n");
 
     sel(a, n);
-     for(i=0;i<n;i++){
+
+    for(i=0; i<n; i++)
+    {
         printf("%d ",a[i]);
     }
-    printf("\n");
+    printf("\n\n");
     return 0;
 }
 
-void sel(int a[], int n){
+void sel(int a[], int n)
+{
 
     int i,j,k;
-    for(i=0;i<n-1;i++){
+    for(i=0; i<n-1; i++)
+    {
         int min = i;
-        for(j=i+1; j<n;j++){
-            if(a[j]<a[min]){
+        for(j=i+1; j<n; j++)
+        {
+            if(a[j]<a[min])
+            {
                 min=j;
             }
         }
-        if(min!=i){
+        if(min!=i)
+        {
             k=a[i];
             a[i]=a[min];
             a[min]=k;
